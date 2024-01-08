4807c464ef5593f1440b395a45898540975ed2f2
Abhijit Sagar <abhijitsagar1204@gmail.com>
2024-01-08 11:27:04 +0530

    this file is missing

---
+++ src/main/java/com/demo/demoProject/DemoProjectApplication.java
@@ -0,0 +1,23 @@
+package com.demo.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+
+	@GetMapping("/demo")
+	public String demo(){
+		return "Hello demo india............";
+	}
+
+}
